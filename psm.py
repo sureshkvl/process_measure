e29a6257d7a10e18fad96565ec66940c845d577d
suresh kumar <sureshkumarr.s@gmail.com>
2016-11-08 04:57:46 +0100

    second commit

--- psm.py
+++ psm.py
@@ -5,7 +5,7 @@ from threading import Timer
 from time import sleep
 import time
 
-
+# second commit
 class RepeatedTimer(object):
     def __init__(self, interval, function, *args, **kwargs):
         self._timer = None
